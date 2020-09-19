82a4b92d08394401daa6edc2cbbc35fe0e2822b5
Ubuntu <simon@cats-of-fort-blanket.1asa0lr0z4ye5lsng20jq5aczb.fx.internal.cloudapp.net>
2020-09-19 09:30:45 +0000

    updated constants

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,17 @@
 """File to save project-wide constants"""
+import os
 
+# PATHS
 DATA_PATH = "/mnt/data"
+TWITTER_PATH = os.path.join(DATA_PATH, "srf_data", "Twitter")
+SMD_PATH = os.path.join(DATA_PATH, "srf_data", "SMD")
+
+# API Keys
+TWITTER_AUTH = {  # Keys for twitter API
+    "API_KEY": "uhiTDzZBuv96kLZVVXFMkfCYh",
+    "API_SECRET": "XepYPEH1mKSpv0QrWyvZt6a64dXCKelAo6cPVCTnONnpS641Oj",
+    "BEARER_TOKEN": "AAAAAAAAAAAAAAAAAAAAAHhhHwEAAAAA0AHrU5ks3gu2cgiht5CqvFr3Sx8%3DB0orQFLuiclY4HWiHWPWCtYLXfXL71HGxAg9SiwyM92m5regRa",
+    "ACCESS_TOKEN": "1255778628614475777-KG9ijrN1oO7R5treIJFr7X9GMQiAPH",
+    "ACCESS_TOKEN_SECRET": "A6edZOelCakLWtDST0bf5JfADeNI1ONQe3Ke2rymuIy5a",
+}
+MS_TEXTANALYSIS = {"API_KEY": "e9d6bb4aaf3c41c3ba2fd90227c1d8bf"}
